f55bc4878e2a5e2fbbd4a32d23605f6991ad0194
vincent08330 <55847636+vincent08330@users.noreply.github.com>
2019-11-26 16:04:01 +0100

    version 2.0

---
+++ BIG PROJECT.py
@@ -0,0 +1,111 @@
+﻿import pygame
+from pygame.locals import *                                             # pygame import
+
+pygame.init()
+taille_fenetre = (775,750)                                              # gestion fenetre
+fenetre_rect = pygame.Rect((0,0), taille_fenetre)
+screen_surface = pygame.display.set_mode(taille_fenetre)
+
+bleu_nuit = (125, 125, 125)
+vert      = (  0,   0,   0)                                             # set up var couleurs
+rouge     = (255,   0,   0)
+bleu      = ( 48, 159, 236)
+
+timer = pygame.time.Clock()
+
+p1 = pygame.Surface((25, 25))                                            # p1 = player 1   #taille
+p1.fill(rouge)
+
+x, y = 500, 500                                                         #position + vitesse
+vx, vy = 0, 0
+
+p2 = pygame.Surface((25, 25))                                            # p2 = player 2   # taille
+p2.fill(bleu)
+
+x2, y2 = 250, 500                                                         #position + vitesse
+vx2, vy2 = 0, 0
+
+mur = pygame.Surface((25, 25))                                           #taille blocks
+mur.fill(vert)
+
+niveau = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],             # visuel niveau
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ]
+
+def dessiner_niveau(surface, niveau):                                                               #cases
+
+    for j, ligne in enumerate(niveau):
+        for i, case in enumerate(ligne):                                            # gestion apparitions blocks
+            if case == 1:
+                surface.blit(mur, (i*25, j*25))                                     # taille block
+
+
+continuer = True
+                                                                                        # alt +f4 = quit
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT :
+            continuer = False
+        elif event.type == KEYDOWN:
+            if event.key == K_SPACE:
+
+                timer.tick(30)                                                                      #clock
+    keys_pressed = pygame.key.get_pressed()
+    vx = (keys_pressed[K_RIGHT] - keys_pressed[K_LEFT]) * 0.35                           #keys moves
+    vy = (keys_pressed[K_DOWN] - keys_pressed[K_UP]) * 0.35
+    vy = min(20, vy)
+    x += vx
+    y += vy                                                                             #vitesse
+    y  = min(725,  y)
+    y  = max(  0,  y)
+    x  = max(  x,   0)                                                                  #maxs et mins
+    x  = min(  x, 725)
+
+    vx2 = (keys_pressed[K_d] - keys_pressed[K_a]) * 0.35                           #keys moves
+    vy2 = (keys_pressed[K_s] - keys_pressed[K_w]) * 0.35
+    vy2 = min(20, vy2)
+    x2 += vx2
+    y2 += vy2                                                                             #vitesse
+    y2  = min(725,  y2)
+    y2  = max(  0,  y2)
+    x2  = max(  x2,   0)                                                                  #maxs et mins
+    x2  = min(  x2, 725)
+
+
+
+    screen_surface.fill(bleu_nuit)                                                      # couleur fenetre
+    dessiner_niveau(screen_surface, niveau)
+    screen_surface.blit(p1, (x, y))                 # affichage p1
+    screen_surface.blit(p2, (x2, y2))                 # affichage p2
+    pygame.display.flip()
+
+pygame.quit()
