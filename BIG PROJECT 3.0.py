202b02df1742c82ebeaba88f6150196ac3849a43
vincent08330 <55847636+vincent08330@users.noreply.github.com>
2019-12-02 08:41:29 +0100

    Update BIG PROJECT 3.0.py

--- BIG PROJECT 3.0.py
+++ BIG PROJECT 3.0.py
@@ -1,17 +1,17 @@
-﻿from tkinter import *                                                # apport tk
+from tkinter import *                                                # apport tk
 
 bleu_nuit = (125, 125, 125)
 vert      = ( 30, 200,  82)                                             # set up var couleurs
 rouge     = (255,   0,   0)
 bleu      = ( 48, 159, 236)
-violet     = (125,   0,  50)
+jaune     = (125,   0,  50)
 
 
 menu = Tk()                                                             # taille fenetre
 
 menu.geometry("200x100")
 
-menu["bg"] = "light blue"                                               #couleur fond
+menu["bg"] = "light blue"
 
 
 Titre = Label(menu, text="Select your game mode:")                      # titre / couleur
@@ -27,49 +27,47 @@ var_jeu2 = IntVar()
 jeu2 = Checkbutton(menu, text="Tower defense", variable = var_jeu2)     # case 2 à cocher
 jeu2.pack()
 
-bouton_quitter = Button(menu, text="Valider", command=menu.quit)        #case quitter
+bouton_quitter = Button(menu, text="Valider", command=menu.quit)
 bouton_quitter.pack()
 
-menu.mainloop()                                                         #boucle pour actualiser Tk
-menu.destroy()                                                          #destruction fentre apres utilisation pour eviter cadavre
+menu.mainloop()
+menu.destroy()
 
 
 
-if (var_jeu2.get() == 1):                                                   # si case 2 cochée
+if (var_jeu2.get() == 1):                                                    # si case 2 cochée
 
     import pygame
     from pygame.locals import *                                             # pygame import
 
-    version = "3.0"
     pygame.init()
-    taille_fenetre = (750,775)                                              # taille fenetre (x/y)
-    pygame.display.set_caption("BIG PROJECT"+ version)
-    fenetre_rect = pygame.Rect((0, 0), taille_fenetre)                     #jsplus
-    screen_surface = pygame.display.set_mode(taille_fenetre)               #surface = taille fenetre
+    taille_fenetre = (750,775)                                             # gestion fenetre
+    fenetre_rect = pygame.Rect((0, 0), taille_fenetre)
+    screen_surface = pygame.display.set_mode(taille_fenetre)
 
-    timer = pygame.time.Clock()                                            #setup clock
+    timer = pygame.time.Clock()
 
-    p1 = pygame.Surface((25, 25))                                          # p1 = player 1   #taille
+    p1 = pygame.Surface((25, 25))                                            # p1 = player 1   #taille
     p1.fill(rouge)
 
-    x, y = 500, 500                                                        #position + vitesse de base
+    x, y = 500, 500                                                         #position + vitesse
     vx, vy = 0, 0
 
-    p2 = pygame.Surface((25, 25))                                          # p2 = player 2   # taille
+    p2 = pygame.Surface((25, 25))                                            # p2 = player 2   # taille
     p2.fill(bleu)
 
-    x2, y2 = 250, 500                                                      #position + vitesse
+    x2, y2 = 250, 500                                                         #position + vitesse
     vx2, vy2 = 0, 0
 
-    pistol1 = pygame.Surface((5,15))                                       #coords de bse du pistolet
-    pistol1.fill(violet)                                                    #couleur
+    pistol1 = pygame.Surface((5,15))
+    pistol1.fill(jaune)
 
-    direction_x1 = x+10                                                    #direction pistol en x
-    direction_y1 = y-10                                                    #direction pistol en y
+    direction_x1 = x+10
+    direction_y1 = y-10
 
 
-    pistol2 = pygame.Surface((5,15))                                       #taille pistolet j2
-    pistol2.fill(violet)                                                    #couleur pistolet j2
+    pistol2 = pygame.Surface((5,15))
+    pistol2.fill(jaune)
 
     direction_x2 = x2+10
     direction_y2 = y2-10
@@ -77,7 +75,7 @@ if (var_jeu2.get() == 1):                                                   # si
 
 
     mur = pygame.Surface((25, 25))                                           #taille blocks
-    mur.fill(vert)                                                           #couleur blocks
+    mur.fill(vert)
 
     niveau = [
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -113,95 +111,99 @@ if (var_jeu2.get() == 1):                                                   # si
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
       ]
 
-    def dessiner_niveau(surface, niveau):                                               #cases
+    def dessiner_niveau(surface, niveau):                                                               #cases
 
       for j, ligne in enumerate(niveau):
-           for i, case in enumerate(ligne):                                             # gestion apparitions blocks
+           for i, case in enumerate(ligne):                                            # gestion apparitions blocks
              if case == 1:
-                 surface.blit(mur, (i*25, j*25))                                        # taille block
+                 surface.blit(mur, (i*25, j*25))                                     # taille block
 
 
 
     continuer = True
 
-    while continuer == True :                                                           # boucle du jeu
-        for event in pygame.event.get():                                                # sortie de secours
+    while continuer == True :
+        for event in pygame.event.get():
             if event.type == QUIT :
                 continuer = False
 
-        timer.tick(60)                                                                  #clock
+        timer.tick(60)                                                                      #clock
         keys_pressed = pygame.key.get_pressed()
-        vx = (keys_pressed[K_RIGHT] - keys_pressed[K_LEFT]) * 3                         #keys moves
+        vx = (keys_pressed[K_RIGHT] - keys_pressed[K_LEFT]) * 3                          #keys moves
         vy = (keys_pressed[K_DOWN] - keys_pressed[K_UP]) * 3
         x += vx
-        y += vy                                                                         #vitesse (ajout de la vitesse à la postion)
+        y += vy                                                                             #vitesse
         y  = min(725,  y)
         y  = max(  0,  y)
-        x  = max(  x,   0)                                                              #maxs et mins
+        x  = max(  x,   0)                                                                  #maxs et mins
         x  = min(  x, 725)
 
 
         if vx > 1:
             pistol1 = pygame.Surface((15,5))
-            pistol1.fill(violet)
-            direction_x1 = x+20                             #gestion orientation pistolet droite
+            pistol1.fill(jaune)
+            direction_x1 = x+20
             direction_y1 = y+10
 
         elif vx < 0:
             pistol1 = pygame.Surface((15,5))
-            pistol1.fill(violet)
-            direction_x1 = x-10                             #gestion orientation pistolet gauche
+            pistol1.fill(jaune)
+            direction_x1 = x-10
             direction_y1 = y+10
 
 
         if vy > 1:
             pistol1 = pygame.Surface((5,15))
-            pistol1.fill(violet)
-            direction_x1 = x+10                             #gestion orientation pistolet bas
+            pistol1.fill(jaune)
+            direction_x1 = x+10
             direction_y1 = y+20
 
         elif vy < 0:
             pistol1 = pygame.Surface((5,15))
-            pistol1.fill(violet)
-            direction_x1 = x+10                              #gestion orientation pistolet haut
+            pistol1.fill(jaune)
+            direction_x1 = x+10
             direction_y1 = y-10
 
-        xpistol1, ypistol1 = direction_x1, direction_y1      #coords pistol j1
+        xpistol1, ypistol1 = direction_x1, direction_y1
 
 
 
-        vx2 = (keys_pressed[K_d] - keys_pressed[K_a]) * 3        # pas commenté = pareil mais pour j2                      
+
+
+
+
+        vx2 = (keys_pressed[K_d] - keys_pressed[K_a]) * 3                                    #keys moves
         vy2 = (keys_pressed[K_s] - keys_pressed[K_w]) * 3
         x2 += vx2
-        y2 += vy2                                                              
+        y2 += vy2                                                                             #vitesse
         y2  = min(725,  y2)
         y2  = max(  0,  y2)
-        x2  = max(  x2,   0)                                                                 
+        x2  = max(  x2,   0)                                                                  #maxs et mins
         x2  = min(  x2, 725)
 
 
         if vx2 > 1:
             pistol2 = pygame.Surface((15,5))
-            pistol2.fill(violet)
+            pistol2.fill(jaune)
             direction_x2 = x2+20
             direction_y2 = y2+10
 
         elif vx2 < 0:
             pistol2 = pygame.Surface((15,5))
-            pistol2.fill(violet)
+            pistol2.fill(jaune)
             direction_x2 = x2-10
             direction_y2 = y2+10
 
 
         if vy2 > 1:
             pistol2 = pygame.Surface((5,15))
-            pistol2.fill(violet)
+            pistol2.fill(jaune)
             direction_x2 = x2+10
             direction_y2 = y2+20
 
         elif vy2 < 0:
             pistol2 = pygame.Surface((5,15))
-            pistol2.fill(violet)
+            pistol2.fill(jaune)
             direction_x2 = x2+10
             direction_y2 = y2-10
 
@@ -210,26 +212,29 @@ if (var_jeu2.get() == 1):                                                   # si
 
 
 
+        screen_surface.fill(bleu_nuit)                                                   # couleur fenetre
+        dessiner_niveau(screen_surface, niveau)
+        screen_surface.blit(p1, (x, y))                                                  # affichage p1
+        screen_surface.blit(p2, (x2, y2))
+        screen_surface.blit(pistol1, (xpistol1, ypistol1))
+        screen_surface.blit(pistol2, (xpistol2, ypistol2))                                                # affichage p2
+        pygame.display.flip()
 
-        screen_surface.fill(bleu_nuit)                             #couleur fond niveau
-        dessiner_niveau(screen_surface, niveau)                    #apparition niveau
-        screen_surface.blit(p1, (x, y))                            #apparition j1
-        screen_surface.blit(p2, (x2, y2))                          #apparition j2
-        screen_surface.blit(pistol1, (xpistol1, ypistol1))         #apparition pistol1
-        screen_surface.blit(pistol2, (xpistol2, ypistol2))         #apparition pistol2        
-
-        if keys_pressed[K_e] > 0:                                  #conidition pour futur menu
+        if keys_pressed[K_e] > 0:
             print("okkk")
 
-        if keys_pressed[K_SPACE] > 0:                              #condition pour futur tirs
+        if keys_pressed[K_SPACE] > 0:
             print("pew pew")
 
 
-    pygame.quit()                                                  #fermeture pygame
+    pygame.quit()
 
 
 
 
-if (var_jeu1.get() == 1):                                          #case choche 1 : 1v1 kohi
+if (var_jeu1.get() == 1):
 
-    print("youpiiiiiiiiii")                                        # pour pas que la variable soit vide
+    print("youpiiiiiiiiii")
+
+
+
